e31910167e1a28e21d054f9078f88a491aac31d0
Costan Daniel <danielcostan99@gmail.com>
2023-09-21 18:08:10 +0300

    finished app

--- src/main/java/ro/scoalainformala/ActivityTracker.java
+++ src/main/java/ro/scoalainformala/ActivityTracker.java
@@ -3,30 +3,33 @@ package ro.scoalainformala;
 import ro.scoalainformala.business.Service;
 import ro.scoalainformala.menu.Menu;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ActivityTracker {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> days = new ArrayList<String>();
+        ArrayList<Integer> steps = new ArrayList<Integer>();
 
         int option;
         do {
             System.out.println(Menu.createMenu());
             option = scanner.nextInt();
             // switch expression
-            String result = switch (option) {
-                case 1 -> Service.addNewActivity();
-                case 2 -> "";
-                case 3 -> "";
-                case 4 -> "";
-                case 5 -> "";
-                case 6 -> "";
-                case 7 -> "";
-                case 8 -> "Goodbye!";
-                default -> "Invalid option";
+            switch (option) {
+                case 1 -> Service.addNewStepsPerDay(steps, days);
+                case 2 -> Service.listAllSteps(steps,days);
+                case 3 -> Service.displayStepsForAGivenDay(steps, days);
+                case 4 -> Service.updateStepsForAGivenDay(steps, days);
+                case 5 -> Service.theMostActiveDay(steps, days);
+                case 6 -> Service.theLeastActiveDay(steps,days);
+                case 7 -> Service.averageSteps(steps);
+                case 8 -> System.out.println("Goodbye!");
+                default -> System.out.println("Invalid option");
             };
-            System.out.println(result);
         } while (option != 8);
     }
 }
--- src/main/java/ro/scoalainformala/business/Service.java
+++ src/main/java/ro/scoalainformala/business/Service.java
@@ -1,9 +1,88 @@
 package ro.scoalainformala.business;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Service {
 
-    public static String addNewActivity() {
-        // TODO implement add functionality
-        return "Activity added";
+    public static void addNewStepsPerDay(ArrayList<Integer> steps, ArrayList<String> days) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number of steps u want to add: ");
+        int stepsRead = Integer.valueOf(scanner.nextLine());
+        System.out.println("Please enter the day that the steps were made: ");
+        String day = scanner.nextLine();
+        steps.add(stepsRead);
+        days.add(day);
+        System.out.println("Thanks for adding! Good bye!");
+    }
+
+    public static void listAllSteps(ArrayList<Integer> steps,ArrayList<String> days) {
+        for (int i = 0;i<=steps.size() -1 ;i++){
+            System.out.println(steps.get(i) + " - " + days.get(i));
+        }
+    }
+
+    public static void displayStepsForAGivenDay(ArrayList<Integer> steps, ArrayList<String> days) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please specify the day: ");
+        String day = scanner.nextLine();
+        if (days.contains(day)) {
+            int index = days.indexOf(day);
+            if (index != -1) {
+                System.out.println(steps.get(index));
+            } else {
+                System.out.println("Day not found!!");
+            }
+
+        }
+    }
+    public static void updateStepsForAGivenDay(ArrayList<Integer> steps, ArrayList<String> days) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please specify the day: ");
+        String day = scanner.nextLine();
+        System.out.println("Please specify the number of steps u want modified:");
+        int step = scanner.nextInt();
+        if (days.contains(day)) {
+            int index = days.indexOf(day);
+            if (index != -1) {
+                steps.set(index,step);
+                System.out.println("Steps we're updated, have a great day! ");
+            } else {
+                System.out.println("Day not found!!");
+            }
+
+        }
+    }
+    public static void theMostActiveDay(ArrayList<Integer> steps, ArrayList<String> days){
+        int mostActive = steps.get(0);
+        int index = 0;
+        for(int step:steps) {
+            if(step >= mostActive){
+                mostActive = step;
+                index = steps.indexOf(step);
+            }
+        }
+        System.out.println("The most active day had " + mostActive + " at the date: " + days.get(index));
+
+    }
+    public static void theLeastActiveDay(ArrayList<Integer> steps, ArrayList<String> days){
+        int leastActive = steps.get(0);
+        int index = 0;
+        for(int step:steps) {
+            if(step <= leastActive){
+                leastActive = step;
+                index = steps.indexOf(step);
+            }
+        }
+        System.out.println("The least active day had " + leastActive + " at the date: " + days.get(index));
+
+    }
+    public static void averageSteps(ArrayList<Integer> steps){
+        int sum = 0;
+        for(int step:steps){
+            sum+=step;
+        }
+        System.out.println(sum*1.0/steps.size());
     }
 }
+
